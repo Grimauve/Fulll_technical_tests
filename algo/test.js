257b1978b827596684bff8a2da9710f813944cca
Clara Grimault <grimault.clara@gmail.com>
2025-05-12 10:46:56 +0200

    Fix testuite

--- algo/test.js
+++ algo/test.js
@@ -4,23 +4,25 @@ import { fizzBuzz } from "./fizzbuzz.js";
 function testFizzBuzzOutput(input, expectedOutput) {
     const logs = [];
     const originalLog = console.log;
-    
+
     console.log = (msg) => logs.push(msg);
-    
+
     fizzBuzz(input);
-    
+
     console.log = originalLog;
-    
-    if (logs[0] === expectedOutput) {
-        console.log(`Test with ${input} passed`);
+
+    const res = logs.join("\n");
+
+    if (res === expectedOutput) {
+        console.log(`✓ Test with ${input} passed`);
     } else {
-        console.log(`Test with ${input} failed`);
-        console.log(`GOT :\n${logs[0]}\nEXPECTED :\n${expectedOutput}`);
+        console.log(`X Test with ${input} failed`);
+        console.log(`GOT :\n${res}\nEXPECTED :\n${expectedOutput}`);
     };
 }
 
 testFizzBuzzOutput(1, '1');
-testFizzBuzzOutput(3, '1 2 Fizz');
-testFizzBuzzOutput(5, '1 2 Fizz 4 Buzz');
-testFizzBuzzOutput(10, '1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz');
-testFizzBuzzOutput(15, '1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz');
+testFizzBuzzOutput(3, '1\n2\nFizz');
+testFizzBuzzOutput(5, '1\n2\nFizz\n4\nBuzz');
+testFizzBuzzOutput(10, '1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz');
+testFizzBuzzOutput(15, '1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\n13\n14\nFizzBuzz');
