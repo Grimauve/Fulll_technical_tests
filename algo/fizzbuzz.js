9eac25ede421a9539ad1992befa2dc01ad552151
Clara Grimault <grimault.clara@gmail.com>
2025-05-12 10:47:41 +0200

    Implement FizzBuzz function

--- algo/fizzbuzz.js
+++ algo/fizzbuzz.js
@@ -1,3 +1,13 @@
 export function fizzBuzz(N) {
-    console.log(`${N.toString()}`);
+    for (let i = 1; i <= N; i++) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            console.log('FizzBuzz');
+        } else if (i % 3 == 0) {
+            console.log('Fizz');
+        } else if (i % 5 == 0) {
+            console.log('Buzz');
+        } else {
+            console.log(`${i.toString()}`);
+        }
+    }
 }
